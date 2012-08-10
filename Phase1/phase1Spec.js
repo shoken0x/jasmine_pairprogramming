d2bf9ed026ea3efa70c93070791a33f5e9878766
syokenz <syokenz@gmail.com>
2012-08-10 19:36:20 +0900

    create phase1

---
+++ Phase1/phase1Spec.js
@@ -0,0 +1,10 @@
+describe('Boling', function() {
+	it('All Gater total 0', function() {
+		expect(0).toEqual(0);
+	});
+
+	it('All 1 pin total 20', function() {
+		expect(20).toEqual(20);
+	});
+});
+
